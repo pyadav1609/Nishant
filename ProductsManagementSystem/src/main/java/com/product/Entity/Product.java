8b97c9fabd5980409145da3b90d840cd8495a61b
ngactive99 <ngactive99@gmail.com>
2022-09-23 09:11:55 +0530

    added main training files

--- ProductsManagementSystem/src/main/java/com/product/Entity/Product.java
+++ ProductsManagementSystem/src/main/java/com/product/Entity/Product.java
@@ -14,39 +14,47 @@ public class Product {
 	private String productName;
 	private String productBrand;
 	private String productCategory;
-	private Integer	productPrice;
-	
-	public int getId() {
+	private Integer productPrice;
+
+	public Integer getId() {
 		return id;
 	}
-	public void setId(int id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public String getProductBrand() {
 		return productBrand;
 	}
+
 	public void setProductBrand(String productBrand) {
 		this.productBrand = productBrand;
 	}
+
 	public String getProductCategory() {
 		return productCategory;
 	}
+
 	public void setProductCategory(String productCategory) {
 		this.productCategory = productCategory;
 	}
+
 	public int getProductPrice() {
 		return productPrice;
 	}
+
 	public void setProductPrice(int productPrice) {
 		this.productPrice = productPrice;
 	}
-	
 
 	public Product(Integer id, String productName, String productBrand, String productCategory, Integer productPrice) {
 		super();
@@ -56,9 +64,9 @@ public class Product {
 		this.productCategory = productCategory;
 		this.productPrice = productPrice;
 	}
+
 	public Product() {
 		super();
-	} 
-		
-	
+	}
+
 }
