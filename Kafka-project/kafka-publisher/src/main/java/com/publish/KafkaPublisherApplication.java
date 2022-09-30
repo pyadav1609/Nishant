1aa5aa0d3e12b8e9583de0b78842da9f02fa3dd0
ngactive99 <ngactive99@gmail.com>
2022-09-30 12:22:00 +0530

    completed kafka producer and added commands

--- Kafka-project/kafka-publisher/src/main/java/com/publish/KafkaPublisherApplication.java
+++ Kafka-project/kafka-publisher/src/main/java/com/publish/KafkaPublisherApplication.java
@@ -1,5 +1,6 @@
 package com.publish;
 
+import org.apache.el.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.publish.entity.User;
+
 @RestController
 @SpringBootApplication
 public class KafkaPublisherApplication {
@@ -15,7 +18,7 @@ public class KafkaPublisherApplication {
 	@Autowired
 	private KafkaTemplate<String, Object> template;
 
-	private String topic = "spring-kafka";
+	private String topic = "spring-kafka1";
 
 	@GetMapping("/publish/{name}")
 	public String publishMessage(@PathVariable String name) {
@@ -23,7 +26,20 @@ public class KafkaPublisherApplication {
 		return "Data has been published";
 
 	}
-
+	
+	
+	@GetMapping("/publishjson")
+	public String publishMessage() {
+		User user = new User(101, "Nishant Gupta", new String[] {"Bangalore", "White field", "Prestige tech park"});
+		template.send(topic, user);
+		return "JSon data has been published";
+	}
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaPublisherApplication.class, args);
 	}
