496d5c3c6eb58aa3c4ba9d281a815da6c2da092e
ngactive99 <ngactive99@gmail.com>
2022-10-11 13:02:39 +0530

    post method done (added student)

--- Angular-files/starter-app/src/app/Services/students.service.ts
+++ Angular-files/starter-app/src/app/Services/students.service.ts
@@ -1,15 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-const BASE_URL= "http://localhost:5000/students/"
+const BASE_URL = 'http://localhost:5000/students/';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudentsService {
-  createStudent(student: {firstname:String, lastname:String, age:number, class:number, subject:String, gender:String}) 
-  { return this.http.post(BASE_URL, student) ;
 
+  createStudent(student: {
+    firstname: String;
+    lastname: String;
+    age: number;
+    class: number;
+    subject: String;
+    gender: String;
+  }) {
+    return this.http.post(BASE_URL, student);
   }
 
-  constructor( public http: HttpClient) { }
+  constructor(public http: HttpClient) {}
 }
--- Angular-files/starter-app/src/app/components/userform/userform.component.ts
+++ Angular-files/starter-app/src/app/components/userform/userform.component.ts
@@ -5,15 +5,25 @@ import { StudentsService } from 'src/app/Services/students.service';
 @Component({
   selector: 'app-userform',
   templateUrl: './userform.component.html',
-  styleUrls: ['./userform.component.css']
+  styleUrls: ['./userform.component.css'],
 })
 export class UserformComponent implements OnInit {
-
   student: Student = new Student();
 
-  constructor( public studentService: StudentsService) { }
-
-  ngOnInit(): void {
+  save() {
+    const observables = this.studentService.createStudent(this.student);
+    observables.subscribe(
+      (response: any) => {
+        console.log(response);
+      },
+      function (error) {
+        console.log(error);
+        alert('Something went wrong try again!');
+      }
+    );
   }
 
+  constructor(public studentService: StudentsService) {}
+
+  ngOnInit(): void {}
 }
