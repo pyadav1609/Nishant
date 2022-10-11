7b6c86950c8bf515420bc3e0bde3e56987e4ac7c
ngactive99 <ngactive99@gmail.com>
2022-10-11 09:20:29 +0530

    day 2

--- Angular-files/starter-app/src/app/app.component.ts
+++ Angular-files/starter-app/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello world';
-  desc:String = "You use the Angular CLI to create projects, generate application and library code, and perform a variety of ongoing development tasks such as testing, bundling, and deployment"
+
+
 }
--- Angular-files/starter-app/src/app/app.module.ts
+++ Angular-files/starter-app/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserformComponent } from './components/userform/userform.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserformComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
