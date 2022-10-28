624fb26dc952296208ee3162ef97151ca202ecaa
ngactive99 <ngactive99@gmail.com>
2022-10-28 10:36:42 +0530

    added a method in the aws deploy demo spring app  before we build it to deploy on ecs

--- spring-aws-deploy/src/main/java/com/deploy/SpringAwsDeployApplication.java
+++ spring-aws-deploy/src/main/java/com/deploy/SpringAwsDeployApplication.java
@@ -3,6 +3,7 @@ package com.deploy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,6 +17,11 @@ public class SpringAwsDeployApplication {
 	@GetMapping("/hello")
 	public String helloWorld() {
 		return "hello my name is Nishant" ;
+	}
+	
+	@GetMapping("/{name}")
+	public String welcome(@PathVariable String name) {
+		return "Hi " + name;
 	}
 
 }
