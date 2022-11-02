6ece813e228ef3dd5eb848a4bf4cd731d34fdce6
ngactive99 <ngactive99@gmail.com>
2022-11-02 11:41:19 +0530

    added project for sns

---
+++ spring-AWS-SNS-demo/src/main/java/com/sns/SpringAwsSnsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsSnsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsSnsDemoApplication.class, args);
+	}
+
+}
---
+++ spring-AWS-SNS-demo/src/main/java/com/sns/config/AWSSNSConfig.java
@@ -0,0 +1,23 @@
+package com.sns.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+
+@Configuration
+public class AWSSNSConfig {
+	
+	@Primary
+	@Bean
+	public AmazonSNSClient getSnsClient() {
+		return (AmazonSNSClient) AmazonSNSClientBuilder.standard().withRegion(Regions.US_EAST_1)
+				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("", ""))).build();
+	}
+
+}
